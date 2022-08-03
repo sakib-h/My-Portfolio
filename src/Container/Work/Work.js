efd8709bfb4c0c1fb2738001acabdb9a998408e6
Sakib Hasan <sakib100.sa@gmail.com>
2022-08-03 12:13:28 +0600

    updating work.js

--- src/Container/Work/Work.js
+++ src/Container/Work/Work.js
@@ -12,7 +12,7 @@ const Work = () => {
 	const [filterWork, setFilterWork] = useState([]);
 
 	useEffect(() => {
-		const query = `*[_type == "works"]{title,description,imgUrl{asset->{_id,url}}}`;
+		const query = `*[_type == "works"]{title,description,projectLink, codeLink, imgUrl{asset->{_id,url}}}`;
 		client.fetch(query).then((data) => {
 			console.log(data);
 			setWorks(data);
@@ -64,6 +64,38 @@ const Work = () => {
 								src={work.imgUrl.asset.url}
 								alt={work.title}
 							/>
+							<motion.div
+								whileHover={{ opacity: [0, 1] }}
+								transition={{
+									duration: 0.25,
+									ease: "easeInOut",
+									staggerChildren: 0.5,
+								}}
+								className="app__work-hover app__flex"
+							>
+								<a
+									href={work.projectLink}
+									target="_blank"
+									rel="noreferrer"
+								>
+									<motion.div
+										whileInView={{
+											scale: [0, 1],
+										}}
+										whileHover={{
+											scale: [
+												1, 0.9,
+											],
+										}}
+										transition={{
+											duration: 0.25,
+										}}
+										className="app__flex"
+									>
+										<AiFillEye />
+									</motion.div>
+								</a>
+							</motion.div>
 						</div>
 					</div>
 				))}
